dc6e4c27a0262058387795bad20d856b7d3ec30c
Hector Hernandez <117788780+Hector3008@users.noreply.github.com>
2024-06-06 12:58:43 -0500

    feature: psbOnCatalogExtractor. Now I have an script which could carrel all the catalog page and It could extract the value of href on its img

---
+++ psbOnCatalog/psbOnCatalog.js
@@ -0,0 +1,29 @@
+console.log("PSBonCatalog running!");
+
+let products = document.querySelectorAll(".product-small.box");
+
+// Convertir NodeList a Array ⬇️⬇️⬇️
+products = Array.from(products);
+
+let savedProducts = products;
+
+let updatableProductList = []
+
+savedProducts.forEach((product) => {
+  let productData = {
+    category: undefined,
+    description: undefined,
+    thumbnail: [],
+  };
+
+  //extraigo los valores del producto desde los children en mi htmlElement
+  productData.category = product.children[1].children[0].children[0].innerText;
+  productData.description =
+    product.children[1].children[0].children[1].innerText;
+  productData.thumbnail.push(
+    product.children[0].children[0].children[0].attributes.href.value
+  );
+  updatableProductList.push(productData)
+});
+
+console.log("updatableProductList: ", updatableProductList);
